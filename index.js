2e3cf0a9a18da18f2287674cd679ed39667069bf
SpindlySpider <kaihrmt@gmail.com>
2024-02-19 18:40:32 +0000

    basic timer added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import express from "express";
+// import express from "express";
+const express = require("express");
 let app = express();
 const port = 8080;
 
 app.get("/",(request,response) =>{
-  response.sendFile(__dirname+"public/index.html");
+  response.sendFile(__dirname+"/public/index.html");
 });
 app.listen(port, function(){
   console.log('Server started at http://localhost:' + port);
---
+++ public/script/timer.js
@@ -0,0 +1,18 @@
+
+function startTimer(){
+  console.log("timer");
+  let timerContainer = document.querySelector("#timerContainer");
+  let timerText = timerContainer.querySelector("h1");
+  let timerInput = timerContainer.querySelector("input");
+  let index = timerInput.value;
+  let intervalID = setInterval(function(){
+    console.log("running",timerInput.value*1000);
+    timerText.textContent = `${index -= 1}`;
+    if(timerText.textContent == 0){
+      clearInterval(intervalID);
+    }
+  },1000
+  );
+}
+let button = document.querySelector("#timerButton");
+button.addEventListener("click",startTimer);
