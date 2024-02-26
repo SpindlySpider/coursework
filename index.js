d1d56141bac148edd6a736637dc64c71c22ae3c1
SpindlySpider <kaihrmt@gmail.com>
2024-02-26 18:54:41 +0000

    start basic structure of coursework

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const express = require("express");
 let app = express();
 const port = 8080;
-
-app.get("/",(request,response) =>{
-  response.sendFile(__dirname+"/public/index.html");
+app.use(express.static(__dirname + "/public/"));
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/public/index.html");
 });
-app.listen(port, function(){
+app.listen(port, function () {
   console.log('Server started at http://localhost:' + port);
 });
