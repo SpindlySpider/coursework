604dcb61f76f5a86a11a13d184d6d35346cc981f
SpindlySpider <kaihrmt@gmail.com>
2024-03-01 03:24:44 +0000

    updated format of timer

--- public/script/timer.js
+++ public/script/timer.js
@@ -1,19 +1,19 @@
 
-function startTimer(){
-  //make sure can only be called once 
-  console.log("timer");
-  let timerContainer = document.querySelector("#timerContainer");
-  let timerText = timerContainer.querySelector("h1");
-  let timerInput = timerContainer.querySelector("input");
+function startTimer() {
+  // make sure can only be called once
+  console.log('timer');
+  const timerContainer = document.querySelector('#timerContainer');
+  const timerText = timerContainer.querySelector('h1');
+  const timerInput = timerContainer.querySelector('input');
   let index = timerInput.value;
-  let intervalID = setInterval(function(){
-    console.log("running",timerInput.value*1000);
+  const intervalID = setInterval(function () {
+    console.log('running', timerInput.value * 1000);
     timerText.textContent = `${index -= 1}`;
-    if(timerText.textContent == 0){
+    if (timerText.textContent === 0) {
       clearInterval(intervalID);
     }
-  },1000
+  }, 1000,
   );
 }
-let button = document.querySelector("#timerButton");
-button.addEventListener("click",startTimer);
+const button = document.querySelector('#timerButton');
+button.addEventListener('click', startTimer);
