d082863a869dbdb7ef90c3c5e20b7598dc685ff8
SpindlySpider <kaihrmt@gmail.com>
2024-03-05 13:16:10 +0000

    backup for sync

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const express = require("express");
 let app = express();
 const port = 8080;
-app.use(express.static(__dirname + "/public/"));
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/public/index.html");
+  app.use(express.static(__dirname + "/public/"));
+  response.sendFile(__dirname + "/public/new_activity.html");
 });
 app.listen(port, function () {
   console.log('Server started at http://localhost:' + port);
--- public/web_componets/new_activity_menu.js
+++ public/web_componets/new_activity_menu.js
@@ -1,26 +1,76 @@
-class newActivtyMenu extends HTMLElement {
-  constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    shadow.innerHTML = `
+
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+:root{
+  --drag-bar-color:#1e90ff;
+}
+*{
+  font-size:20px;
+  padding : 15px 20px 10px 20px 
+}
+#dragElement{
+  width:50px;
+  height:10px;
+  background-color:red;
+}
+header, main{
+  display:flex;
+  flex-direction: column;
+}
+</style>
 <header>
     <div id="dragElement"></div>
     <div id="activtyMenuHeading">
-        <button id="exit"></button>
+        <button id="exit">exit</button>
         <input id="activityNameInput"></input>
-        <button id="done"></button>
+        <button id="doneButton">done</button>
     </div>
   </header>
   <main>
      <input id="descriptionInput"></input>
     <div class="column-group">
-      <input type="time"></input>
+      <input id="timeInput" type="time"></input>
         <input id="addPhoto"></input>
         <div id="tags"></div>
         <div id="required equipment"></div>
     </div>
       </main>
 `;
+class newActivtyMenu extends HTMLElement {
+  // might be able to abstract this out and then add custom elements to it and use it just as a menu for all things
+  // also if any of the attributes change then we need to update local storage + server cache 
+  constructor() {
+    //must do all of the selections within the constructor
+    super();
+    this.shadow = this.attachShadow({ mode: 'open' });
+    this.shadow.innerHTML = `${template.innerHTML}`;
+    this.donebutton = this.shadow.querySelector("#doneButton");
+  }
+  connectedCallback(){
+    this.donebutton.addEventListener("click",this.saveNewActivty.bind(this));
+    //set up event listners here
+  }
+  disconnectedCallback(){
+  }
+    saveNewActivty(){
+    const title = this.shadow.querySelector("#doneButton").value;
+    const description = this.shadow.querySelector("#descriptionInput").value;
+    const duration = this.shadow.querySelector("#timeInput").value;
+    let newActivty = {
+      "UUID" : crypto.randomUUID(),
+      "title": title ,
+      "description": description,
+      "duration":  duration
+    };
+    newActivty =  JSON.stringify(newActivty);
+    // save in local cache then send to the server 
+    // must request UUID from server 
+    // fall back if server is not reachable is service worker gerneates new UUID
+    //create JSON object to store 
+    // must implment error checking 
+    localStorage["activites"] = newActivty;
+    // localStorage["activites"][crypto.randomUUID()] = newActivty;
   }
 }
-customElements.define('new_activty_menu', newActivtyMenu);
+customElements.define('new-activty-menu', newActivtyMenu);
