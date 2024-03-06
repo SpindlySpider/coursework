92f8cf08295278f4aab2475c6f9d438b79856bfd
spindly__ <kaihrmt@gmail.com>
2024-03-06 20:53:37 +0000

    created category page
    when you click the category button it shows a basic category page now

--- public/script/main.js
+++ public/script/main.js
@@ -1,10 +1,8 @@
-function newActivityPageDisplay() {
-
-}
+function newActivityPageDisplay() {}
 
 function displayNewEntryOptions() {
   if (document.querySelector('#newActivtyPopup') == null) {
-    const newActivty = document.createElement('div');
+    const newActivty = document.createElement('li');
     newActivty.id = 'newActivtyPopup';
     newActivty.textContent = 'create new activity';
     newActivty.classList.add('right-floating-notification');
@@ -14,5 +12,37 @@ function displayNewEntryOptions() {
     document.querySelector('#newActivtyPopup').remove();
   }
 }
+const categeoryTemplate = document.createElement('template');
 
-document.querySelector('#new-entry').addEventListener('click', displayNewEntryOptions);
+categeoryTemplate.innerHTML = `
+<style>
+.categoryElement{
+
+}
+.centerCategory{
+  display:flex;
+
+}
+</style>
+    <nav class="navbar ">
+      <li class="centerCategory navbar-item">list view</li>
+      <li class="centerCategory navbar-item">body view</li>
+      <li class="centerCategory navbar-item">difficulty</li>
+      <li class="rightCategory navbar-item">search</li>
+    </nav>
+    <ul class = "vertical-item-list">
+      <li id = "customActivtiesCategory" class= "categoryElement"> custom activties </li>
+    </ul>
+`;
+
+function displayCategoryPage() {
+  const main = document.querySelector('#main-content');
+  main.innerHTML = categeoryTemplate.innerHTML;
+  // console.log(':)');
+}
+document
+  .querySelector('#new-entry')
+  .addEventListener('click', displayNewEntryOptions);
+document
+  .querySelector('#catagories')
+  .addEventListener('click', displayCategoryPage);
