13951c82e311351ed08a94c58fab5325816d5ea2
spindly__ <kaihrmt@gmail.com>
2024-03-08 03:29:21 +0000

    created comments to easily test

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // import express from "express";
-const express = require("express");
-let app = express();
+const express = require('express');
+const app = express();
 const port = 8080;
-app.get("/", (request, response) => {
-  app.use(express.static(__dirname + "/public/"));
-  response.sendFile(__dirname + "/public/new_activity.html");
+app.get('/', (request, response) => {
+  app.use(express.static(__dirname + '/public/'));
+  response.sendFile(__dirname + '/public/templates/bottomsheet.html');
+  // response.sendFile(__dirname + '/public/index.html');
+  // response.sendFile(__dirname + '/public/new_activity.html');
 });
 app.listen(port, function () {
   console.log('Server started at http://localhost:' + port);
