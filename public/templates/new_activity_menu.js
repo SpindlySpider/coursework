202f004dab4a9f12e787fa316be687fb5e39f2d0
spindly__ <kaihrmt@gmail.com>
2024-03-08 03:26:57 +0000

    exported html && css into template of new activity

---
+++ public/templates/new_activity_menu.js
@@ -0,0 +1,38 @@
+export const newActivtyMenuTemplate = document.createElement('template');
+newActivtyMenuTemplate.innerHTML = `
+<style>
+:root{
+  --drag-bar-color:#1e90ff;
+}
+*{
+  font-size:20px;
+  padding : 15px 20px 10px 20px 
+}
+#dragElement{
+  width:50px;
+  height:10px;
+  background-color:red;
+}
+header, main{
+  display:flex;
+  flex-direction: column;
+}
+</style>
+<header>
+    <div id="dragElement"></div>
+    <div id="activtyMenuHeading">
+        <button id="exit">exit</button>
+        <input id="activityNameInput"></input>
+        <button id="doneButton">done</button>
+    </div>
+  </header>
+  <main>
+     <input id="descriptionInput"></input>
+    <div class="column-group">
+      <input id="timeInput" type="time"></input>
+        <input id="addPhoto"></input>
+        <div id="tags"></div>
+        <div id="required equipment"></div>
+    </div>
+      </main>
+`;
--- public/web_componets/new_activity_menu.js
+++ public/web_componets/new_activity_menu.js
@@ -1,44 +1,5 @@
 import { saveActivty } from './utilities.js';
-
-const template = document.createElement('template');
-
-template.innerHTML = `
-<style>
-:root{
-  --drag-bar-color:#1e90ff;
-}
-*{
-  font-size:20px;
-  padding : 15px 20px 10px 20px 
-}
-#dragElement{
-  width:50px;
-  height:10px;
-  background-color:red;
-}
-header, main{
-  display:flex;
-  flex-direction: column;
-}
-</style>
-<header>
-    <div id="dragElement"></div>
-    <div id="activtyMenuHeading">
-        <button id="exit">exit</button>
-        <input id="activityNameInput"></input>
-        <button id="doneButton">done</button>
-    </div>
-  </header>
-  <main>
-     <input id="descriptionInput"></input>
-    <div class="column-group">
-      <input id="timeInput" type="time"></input>
-        <input id="addPhoto"></input>
-        <div id="tags"></div>
-        <div id="required equipment"></div>
-    </div>
-      </main>
-`;
+import { newActivtyMenuTemplate } from '../templates/new_activity_menu.js';
 class newActivtyMenu extends HTMLElement {
   // might be able to abstract this out and then add custom elements to it and use it just as a menu for all things
   // also if any of the attributes change then we need to update local storage + server cache
@@ -46,7 +7,7 @@ class newActivtyMenu extends HTMLElement {
     // must do all of the selections within the constructor
     super();
     this.shadow = this.attachShadow({ mode: 'open' });
-    this.shadow.innerHTML = template.innerHTML;
+    this.shadow.innerHTML = newActivtyMenuTemplate.innerHTML;
     this.donebutton = this.shadow.querySelector('#doneButton');
   }
 
