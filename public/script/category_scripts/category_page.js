ddb6b340c9c363f26ba05b3c27facceff07a4fa5
spindly__ <kaihrmt@gmail.com>
2024-03-09 00:33:55 +0000

    implemented basic category page

--- public/script/category_scripts/category_page.js
+++ public/script/category_scripts/category_page.js
@@ -1,26 +1,33 @@
-import {
-  categeoryTemplate,
-  customCategeoryTemplate,
-} from '../../templates/category_page.js';
 import { getAllCustomActivites } from '../../web_componets/utilities.js';
 
 export function displayCustomCateogryPage() {
   const main = document.querySelector('#main-content');
-  main.innerHTML = customCategeoryTemplate.innerHTML;
+  const menu = document.createElement('bottom-sheet-menu');
+  main.appendChild(menu);
+  menu.setTitle('custom activties');
   const customActivties = getAllCustomActivites();
-  console.log(customActivties);
   for (let item of Object.keys(customActivties)) {
     // make a web componenet for the event
     const entry = document.createElement('p');
     entry.textContent = customActivties[item].title;
-    main.appendChild(entry);
+    entry.classList.add('bottomsheet-content-item');
+    menu.appendEntry(entry);
   }
 }
 
 export function displayCategoryPage() {
   const main = document.querySelector('#main-content');
-  main.innerHTML = categeoryTemplate.innerHTML;
-  document
+  const menu = document.createElement('bottom-sheet-menu');
+  const customActivties = document.createElement('h1');
+  customActivties.textContent = 'custom activties';
+  customActivties.classList.add('bottomsheet-content-item');
+  customActivties.id = 'customActivtiesCategory';
+  main.appendChild(menu);
+  menu.setTitle('categories');
+  menu.appendEntry(customActivties);
+  // put the web componenet here
+
+  menu.shadow
     .querySelector('#customActivtiesCategory')
     .addEventListener('click', displayCustomCateogryPage);
 }
--- public/templates/category_page.js
+++ public/templates/category_page.js
@@ -1,3 +1,4 @@
+import { bottomSheetTemplate } from '../templates/bottom_sheet.js';
 export const categeoryTemplate = document.createElement('template');
 export const customCategeoryTemplate = document.createElement('template');
 
@@ -5,21 +6,4 @@ customCategeoryTemplate.innerHTML = `
 <h1 id="custom-categories">custom categories</h1>
 `;
 
-categeoryTemplate.innerHTML = `
-<style>
-.categoryElement{
-}
-.centerCategory{
-  display:flex;
-}
-</style>
-    <nav class="navbar ">
-      <li class="centerCategory navbar-item">list view</li>
-      <li class="centerCategory navbar-item">body view</li>
-      <li class="centerCategory navbar-item">difficulty</li>
-      <li class="rightCategory navbar-item">search</li>
-    </nav>
-    <ul class = "vertical-item-list">
-      <li id = "customActivtiesCategory" class= "categoryElement"> custom activties </li>
-    </ul>
-`;
+categeoryTemplate.innerHTML = bottomSheetTemplate.innerHTML;
